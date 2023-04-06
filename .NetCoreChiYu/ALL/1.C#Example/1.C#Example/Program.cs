279afd958dc6e3b681b5c46aed5fa471846dada9
Eric Huang 黃祈豫 <eric.huang@cloudriches.com>
2023-04-06 14:29:53 +0800

    調整檔案結構

---
+++ .NetCoreChiYu/ALL/1.C#Example/1.C#Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.C_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ .NetCoreChiYu/ALL/2.Genaral Develop Skill/2.Genaral Develop Skill/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2.Genaral_Develop_Skill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
